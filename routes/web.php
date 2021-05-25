599c1667ba8904065992b7ad2295b5a9adbbbe26
zaq12wsxzaswq12 <zaq12wsxzaswq12@gmail.com>
2021-05-25 21:50:04 +0900

    OAuth関係の全変更

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('auth.login');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/index', 'App\Http\Controllers\IndexController@index')->name('index');
+
+Route::get('login/google', 'App\Http\Controllers\Auth\LoginController@redirectToGoogle');
+Route::get('login/google/callback', 'App\Http\Controllers\Auth\LoginController@handleGoogleCallback');
